10e0e118c2521ef6107a77b045381768f0c28a38
SebinDuke <sebindukestephan@gmail.com>
2017-06-11 09:28:51 +0530

    Signup-Login

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+
+from django.urls import reverse
+#for older versoins of Django use:
+#from django.core.urlresolvers import reverse
+
 
 def index(request):
     return HttpResponse("Index page")
