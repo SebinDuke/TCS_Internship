b3ef201c72de88facbc45b65a60f9b4dde9c3fb0
Sebin Duke Stephan <sebindukestephan@gmail.com>
2017-06-11 09:04:06 +0530

    Login Functionallity

---
+++ TCS_Internship/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url,include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^', include('main.urls')),
+    url(r'^user', include('login.urls')),
+    url(r'^admin/', admin.site.urls),
+]
