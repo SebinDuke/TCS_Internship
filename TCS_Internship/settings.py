0b49df75e0c7a010c08c1174f88cb2c1dfa63cc5
Rishabh Narang <rnradon17@gmail.com>
2017-07-11 19:27:12 +0530

    pushing to heroku

--- TCS_Internship/settings.py
+++ TCS_Internship/settings.py
@@ -115,5 +115,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
