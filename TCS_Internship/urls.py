16174ad0fce14e69babbfc750702d027652d110d
Rishabh Narang <rnradon17@gmail.com>
2017-07-11 19:36:21 +0530

    pushing to heroku

--- TCS_Internship/urls.py
+++ TCS_Internship/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^', include('main.urls')),
     url(r'^user/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
