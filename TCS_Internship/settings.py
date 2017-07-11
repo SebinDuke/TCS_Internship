7405d0d821a438bf4b22f6b92d5a1deff81c3dad
Rishabh Narang <rnradon17@gmail.com>
2017-07-11 19:42:20 +0530

    pushing to heroku

--- TCS_Internship/settings.py
+++ TCS_Internship/settings.py
@@ -111,9 +111,18 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- TCS_Internship/urls.py
+++ TCS_Internship/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url,include
 from django.contrib import admin
-from . import settings
 
 urlpatterns = [
     url(r'^', include('main.urls')),
     url(r'^user/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    
 ]
