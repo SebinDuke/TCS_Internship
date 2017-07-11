a6716be0e4f89a847f9cca7777d40f07d2c4a968
Rishabh Narang <rnradon17@gmail.com>
2017-07-11 20:10:57 +0530

    favicon added

--- TCS_Internship/urls.py
+++ TCS_Internship/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/images/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^', include('main.urls')),
     url(r'^user/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
-    
+    url(r'^favicon\.ico$', favicon_view),
 ]
