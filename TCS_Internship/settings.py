01f058b944d2e6093e5c2f42255ad4a20df5cb97
Rishabh Narang <rnradon17@gmail.com>
2017-07-11 19:29:53 +0530

    pushing to heroku

--- TCS_Internship/settings.py
+++ TCS_Internship/settings.py
@@ -110,10 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
